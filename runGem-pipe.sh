117e8c9dd521cd8b1627a4aac4a3ca5296c37c8f
ramirocf <ramiro.2093@gmail.com>
2017-03-17 12:21:11 -0600

    Files used to run gem5-pipe

---
+++ runGem-pipe.sh
@@ -0,0 +1,1 @@
+./build/ALPHA/gem5.opt --debug-flags=FaultInjection,DiskImageWrite ./configs/example/fs.py --cpu-type=detailed --caches  --script=./benchmarks/miBench/automotive/susan/gem5_files/runme_small.sh  
